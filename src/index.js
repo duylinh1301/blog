1190ad300c62fd67a75c17c8fbd0b401cc8a73f0
Duy Linh <linh@fuelcloud.com>
2022-06-01 16:37:58 +0700

    add static path and config scss to project

--- src/index.js
+++ src/index.js
@@ -8,16 +8,15 @@ const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Template Engine
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'views'));
-
-console.log(__dirname);
+app.engine('hbs', engine({extname: '.hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home')
 })
 
 app.listen(port, () => {
